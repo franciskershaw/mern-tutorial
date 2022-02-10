a8621c703bf1756394e5923d5dccfe6a57751468
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-02-10 11:58:05 +0000

    test commit

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
 
     res.status(statusCode);
 
-    // Only show stack if in development
+    // Only show stack if in developments
     res.json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
